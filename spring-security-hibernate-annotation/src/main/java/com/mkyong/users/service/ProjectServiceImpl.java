40c73f5235098d7698b3fac614d49d8e6242df4f
celeste <celeste@192.168.1.60>
2018-11-10 17:10:39 +0100

    crear proyecto

--- spring-security-hibernate-annotation/src/main/java/com/mkyong/users/service/ProjectServiceImpl.java
+++ spring-security-hibernate-annotation/src/main/java/com/mkyong/users/service/ProjectServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mkyong.users.service;
 
-import java.util.Date;
+
+
+import java.sql.Date;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
--- spring-security-hibernate-annotation/src/main/java/com/mkyong/web/controller/ProjectController.java
+++ spring-security-hibernate-annotation/src/main/java/com/mkyong/web/controller/ProjectController.java
@@ -1,13 +1,9 @@
 package com.mkyong.web.controller;
 
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,20 +13,15 @@ import org.springframework.web.servlet.ModelAndView;
 import com.mkyong.users.model.DataModel;
 import com.mkyong.users.model.Organization;
 import com.mkyong.users.model.Personal;
-import com.mkyong.users.model.Project;
-import com.mkyong.users.model.User;
 import com.mkyong.users.service.DatamodelService;
 import com.mkyong.users.service.OrganizationService;
 import com.mkyong.users.service.PersonalService;
 import com.mkyong.users.service.ProjectService;
 
+import java.sql.Date;
 import java.text.ParseException;
-
 import java.text.SimpleDateFormat;
 
-import java.util.Date;
-
-import java.util.Locale;
 
 @Controller
 public class ProjectController {
@@ -62,30 +53,28 @@ public class ProjectController {
 			@ModelAttribute("datamodel") int id_datamodel,
 			@ModelAttribute("personal") int id_personal,
 			@ModelAttribute("start_date") String inicio, 
-			@ModelAttribute("finish_date") String finalizar) {
+			@ModelAttribute("finish_date") String finalizar) throws ParseException {
 	
-		SimpleDateFormat formatoDelTexto = new SimpleDateFormat("yyyy-MM-dd");
+		int id = 0;
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date date = sdf1.parse(inicio);
+		java.sql.Date sqlStartDate = new java.sql.Date(date.getTime()); 
+		java.util.Date date1 = sdf1.parse(finalizar);
+		java.sql.Date sqlStartDate1 = new java.sql.Date(date.getTime()); 
+		projectService.addProject(id,id_personal, id_organization, id_datamodel, sqlStartDate,sqlStartDate1);
 		
-		Date start_date = null;
-		Date finish_date = null;
-		
-		try {
+		/*try {
 
-			start_date = formatoDelTexto.parse(inicio);
-			finish_date = formatoDelTexto.parse(finalizar);
+			start_date = (Date) formatoDelTexto.parse(inicio);
+			finish_date = (Date) formatoDelTexto.parse(finalizar);
+			System.out.println("++++++"+start_date+ "" +finish_date);
 			if(start_date != null && finish_date!=null) {
 				int id = 0;
-				projectService.addProject(id,id_personal, id_organization, id_datamodel, start_date,finish_date);
+				//projectService.addProject(id,id_personal, id_organization, id_datamodel, start_date,finish_date);
 			}
-
 		} catch (ParseException ex) {
-
 		ex.printStackTrace();
-
-		}
-
-
-
+		}*/
 		return new ModelAndView("redirect:/main");
 	}
 
